effcf577cbd9bb3c342966e8e2ea137362ace794
Randhaya <randhaya.r431@gmail.com>
2024-11-22 13:58:28 +0100

    WIP: Progress on Login/logout feature

--- webshop/settings.py
+++ webshop/settings.py
@@ -127,3 +127,14 @@ STATICFILES = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# redirect after login
+LOGIN_REDIRECT_URL = '/'
+
+# redirect after logout
+LOGOUT_REDIRECT_URL = '/'
+
+# use django's buit-in User model
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',  # Default authentication backend
+]
