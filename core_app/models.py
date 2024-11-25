338e76a2a3b382a0ab564faf6a96f24b6ef85660
Antonis <tsiapalis.antonis@gmail.com>
2024-11-25 14:36:52 +0100

    deleted UserProfileInfo. There was no need for that

--- core_app/models.py
+++ core_app/models.py
@@ -1,11 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-
-class UserProfileInfo(models.Model):
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_of_birth = models.DateField(blank=True)
-
-    def __str__(self):
-        return f'{self.user.first_name} {self.user.last_name}'
