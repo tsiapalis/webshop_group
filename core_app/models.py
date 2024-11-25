909d13c01c6cc49cc091311cdaab21ca071e66b2
Antonis <tsiapalis.antonis@gmail.com>
2024-11-25 13:25:58 +0100

    added UserProfileInfo at models, RegistrationForm at forms.py, register method in views and a registration html.

--- core_app/models.py
+++ core_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfileInfo(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
--- core_app/views.py
+++ core_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from forms import RegistrationForm
 
 # Create your views here.
 class CustomLoginView(LoginView):
@@ -11,10 +12,24 @@ class CustomLoginView(LoginView):
 class CustomLogoutView(LogoutView):
     next_page = '/'                # redirected to homepage after logout
 
-    
+
 def index(request):
     return render(request, 'core_app/index.html')
 
-
 def about(request):
     return render(request, 'core_app/about.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('login')  # Redirect to the login page after successful registration
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'core_app/register.html', {'form': form})
+
