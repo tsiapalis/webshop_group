f0bf66de9f39e2990750e087f11c9b338339a3e0
Randhaya <randhaya.r431@gmail.com>
2024-11-26 09:59:01 +0100

    Added logout page with review

--- core_app/urls.py
+++ core_app/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('discovery/', views.discovery, name='discovery'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('logout/success/', views.logout_success, name='logout_success'),
+    path('submit_review/', views.submit_review, name='submit_review'),
     path('about/', views.about, name='about'),
     path('registration/', views.register, name='registration')
 ]
