c4da53923b5c7a7cb802befabf973268ee476e23
Antonis <tsiapalis.antonis@gmail.com>
2024-11-28 11:48:37 +0100

    changed dependencies in migrations.

--- core_app/migrations/0005_review.py
+++ core_app/migrations/0005_review.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core_app', '0004_initial'),
+        ('core_app', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
