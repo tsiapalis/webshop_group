8163fd70477fd2113aaaba2dfbc03b850aa896a3
Linus Silfver <linus.silfver@gmail.com>
2024-11-29 15:35:05 +0100

    change category logic

--- core_app/discovery_view.py
+++ core_app/discovery_view.py
@@ -4,15 +4,23 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 
 class Discovery(View):
-    def get(self, request, item_id=None):
+    def get(self, request, item_id=None): # item_id=None,  category=None
+        cart = request.session.get('cart', {})
+        cart_count = sum(cart.values())
+    
         if item_id:
             item = get_object_or_404(Candle, id=item_id)
-            return render(request, 'core_app/discovery/detailed_item.html', {'item': item})
+            return render(request, 'core_app/discovery/detailed_item.html', {'cart_count': cart_count, 'item': item})
         else:
-            cart = request.session.get('cart', {})
-            cart_count = sum(cart.values())
-            items = Candle.objects.all()
-            return render(request, 'core_app/discovery/discovery.html', {'cart_count': cart_count, 'items': items})
+            category = request.GET.get('category', None)
+            print(f"category: {category}")
+            if category:
+                items = Candle.objects.filter(category=category)
+            else:
+                items = Candle.objects.all()
+        
+        return render(request, 'core_app/discovery/discovery.html', {'cart_count': cart_count, 'items': items})
+
 
     def post(self, request): 
         product_id  = request.POST.get('product')
--- core_app/views.py
+++ core_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 
 def index(request):
     cart = request.session.get('cart', {})
@@ -9,19 +9,3 @@ def index(request):
 
 def about(request):
     return render(request, 'core_app/about.html')
-
-class Discovery(TemplateView):
-    template_name = 'core_app/discovery.html'
-
-    def get_context_data(self, **kwargs):
-        print("Discovery view accessed!")
-        selected_category = self.request.GET.get('category')
-
-        candles = Candle.objects.filter(category=selected_category) if selected_category else Candle.objects.all()
-        context = {
-            'candles': candles,
-            'categories': Candle.CATEGORY_CHOICES,  # Categories for the dropdown
-            'selected_category': selected_category,  # Pass selected category for the dropdown
-        }
-        print(context)
-        return context
