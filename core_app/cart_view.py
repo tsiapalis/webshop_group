d662c244e20cc16ef9561777e8ad9014254d0bc9
Linus Silfver <linus.silfver@gmail.com>
2024-12-02 14:11:42 +0100

    fixed cart issues in discovery and cart

--- core_app/cart_view.py
+++ core_app/cart_view.py
@@ -2,20 +2,37 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views import View
 from .models import Candle
+from django.http import HttpResponseRedirect
 
 class CartView(View):
     def get(self, request):
         cart = request.session.get('cart', {})
 
-        print(cart)
-
         items = []
         for id, quantity in cart.items():
             item = get_object_or_404(Candle, id=id)
             items.append({'item': item, 'quantity': quantity})
-
-        print('items: ', items)
-        cart_count = sum(cart.values())
+        print(items)
+        return render(request, 'core_app/cart.html', {'items': items, 'cart_count': sum(cart.values())})
+    
+    def post(self, request):
+        method = self.request.POST.get('method', '').lower()
+        item_id = self.request.POST.get('item_id', '').lower()
+        if method == 'delete':
+            return self.delete(request, item_id)
         
+        quantity  = int(request.POST.get('quantity'))
+        cart = request.session.get('cart', {})
+        cart[item_id] = quantity
 
-        return render(request, 'core_app/cart.html', {'items': items, 'cart_count': cart_count})
+        request.session['cart'] = cart 
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    
+    def delete(self, request, item_id):
+        cart = request.session.get('cart', {})
+        if cart and cart[item_id]:
+            print("in here")
+            cart.pop(item_id)
+            request.session['cart'] = cart 
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
