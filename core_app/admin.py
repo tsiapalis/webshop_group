1972fe79d0b6967cd23632348156e9bab1339cec
Antonis <tsiapalis.antonis@gmail.com>
2024-12-02 13:45:35 +0100

    added the models

--- core_app/admin.py
+++ core_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Candle, Review
+from .models import Candle, Review, Order, OrderItem
 
 # Register your models here.
 admin.site.register(Candle)
 admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
