3ed0ff7f26e8bb61e2787b48d8628085f41baa25
Linus Silfver <linus.silfver@gmail.com>
2024-12-03 16:17:14 +0100

    added form and post to checkout details

--- core_app/views/checkout_view.py
+++ core_app/views/checkout_view.py
@@ -1,8 +1,9 @@
 from django.views import View
 from django.shortcuts import render, get_object_or_404
 from ..models import Candle
+from django.http import HttpResponseRedirect
 
-class CheckoutView(View):
+class DetailsView(View):
     def get(self, request):
         cart = request.session.get('cart', {})    
         
@@ -28,3 +29,32 @@ class CheckoutView(View):
 
         return render(request, 'core_app/checkout/details.html', {'items': items, 'subTotal': subTotal})
     
+
+    def post(self, request):
+        email = request.POST.get('email')
+        newsletter = request.POST.get('newsletter')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        address = request.POST.get('address')
+        shipping_note = request.POST.get('shipping_note')
+        city = request.POST.get('city')
+        postal_code = request.POST.get('postal_code')
+        province = request.POST.get('province')
+        country = request.POST.get('country')
+        save_info = request.POST.get('save_info')
+
+        print({
+            "email": email,
+            "newsletter": newsletter,
+            "first_name": first_name,
+            "last_name": last_name,
+            "address": address,
+            "shipping_note": shipping_note,
+            "city": city,
+            "postal_code": postal_code,
+            "province": province,
+            "country": country,
+            "save_info": save_info,
+        })
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
