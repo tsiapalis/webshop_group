369cd440908ae585396b30ef0690b8fee7076e3b
Antonis <tsiapalis.antonis@gmail.com>
2024-12-05 12:30:01 +0100

    changed and commented out the details part in checkout view. fixed an issue with email in form. Changed view's return to render shipping page. Added a block for adding the order to Order and OrderItem tables

--- core_app/urls.py
+++ core_app/urls.py
@@ -5,7 +5,7 @@ from .views.auth_views import LoginView, RegisterView, ResetPasswordView, logout
 from .views.discovery_view import Discovery
 from .views.review_view import ReviewView
 from .views.cart_view import CartView
-from .views.checkout_view import DetailsView, PaymentView
+from .views.checkout_view import DetailsView, ShippingView, PaymentView
 from .views.profile_view import InfoChangeView, BillingPayments, Settings, TransactionsHistory
 
 
@@ -16,17 +16,11 @@ urlpatterns = [
     path('discovery/', Discovery.as_view(), name='discovery'),
     path('item/<int:item_id>', Discovery.as_view(), name='detailed_item'),
     path('cart/', CartView.as_view(), name='cart'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', logout_view, name='logout'),
     # Auth Views
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
     path('review/', ReviewView.as_view(), name='review'),
     path('about/', views.about, name='about'),
-    path('item/<int:item_id>', Discovery.as_view(), name='detailed_item'),
-    path('registration/', RegisterView.as_view(), name='registration'),
-    path('reset-password/', ResetPasswordView.as_view(), name='reset_password'),
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('registration/', RegisterView.as_view(), name='registration'),
     path('reset-password/', ResetPasswordView.as_view(), name='reset_password'),
     # Settings Views
@@ -37,6 +31,7 @@ urlpatterns = [
     path('my-profile/settings', Settings.as_view(), name='settings'),
     # Checkout Views
     path('checkout/details', DetailsView.as_view(), name='checkout_details'),
+    path('checkout/shipping', ShippingView.as_view(), name='checkout_shipping'),
     path('checkout/shipping', DetailsView.as_view(), name='checkout_shipping'),
     path('checkout/payment', PaymentView.as_view(), name='checkout_payment'),
 ]
--- core_app/views/checkout_view.py
+++ core_app/views/checkout_view.py
@@ -1,24 +1,21 @@
 from django.views import View
 from django.shortcuts import render, get_object_or_404
-from ..models import Candle
+from ..models import Candle, Order, OrderItem, User
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
-class CheckoutView(View):
+class DetailsView(View):
     def get(self, request):
         cart = request.session.get('cart', {})    
-        
+
         subTotal = 0
         items = []
         for id, quantity in cart.items():
             item = get_object_or_404(Candle, id=id)
 
-            if item.in_stock == 0:
-                # Skip items if out of stock
-                continue
-
-            if quantity > item.in_stock:
-                # Change quantity to match the available stock
-                quantity = item.in_stock
-                cart[id] = quantity
+            if item.in_stock == 0 or quantity > item.in_stock:
+                messages.success(request, "The items stock changed.")
+                return HttpResponseRedirect('/cart')
 
             items.append({'item': item, 'quantity': quantity})
             subTotal += item.price * quantity
@@ -28,3 +25,108 @@ class CheckoutView(View):
 
         return render(request, 'core_app/checkout/details.html', {'items': items, 'subTotal': subTotal})
     
+
+    def post(self, request):
+
+        ''' TODO: the 'details' dict will be you used for the shipping address model we will add later.
+
+        # details = request.session.get('details', {})
+
+        # for key in request.POST.keys():
+        #     if key != "csrfmiddlewaretoken":
+        #         details[key] = request.POST.get(key)
+
+        '''
+
+        # Add to Order and OrderItem. I think its better if we move this block to payment view.
+        cart = request.session.get('cart', {})
+
+        order = Order.objects.create(user=request.user)
+        for key, value in cart.items():
+            candle = Candle.objects.get(id=key)
+            OrderItem.objects.create(order=order, item=candle, quantity=value)
+
+        return render(request, 'core_app/checkout/shipping.html')
+
+class ShippingView(View):
+    def get(self, request):
+        cart = request.session.get('cart', {})    
+        
+        subTotal = 0
+        items = []
+        for id, quantity in cart.items():
+            item = get_object_or_404(Candle, id=id)
+
+            if item.in_stock == 0 or quantity > item.in_stock:
+                # TODO: return to cart provide a message in cart
+                HttpResponseRedirect('/cart')
+
+            items.append({'item': item, 'quantity': quantity})
+            subTotal += item.price * quantity
+
+        return render(request, 'core_app/checkout/details.html', {'items': items, 'subTotal': subTotal})
+    
+    def post(self, request):
+        contact = request.POST.get('contact')
+        ship_to = request.POST.get('ship_to')
+        delivery_method = request.POST.get('delivery_method')
+        shipping_cost = 0
+
+        print({
+            "contact": contact,
+            "ship_to": ship_to,
+            "delivery_method": delivery_method,
+            "shipping_cost": shipping_cost,
+        })
+
+        checkout = request.session.get('checkout', {})
+
+        checkout['shipping'] = {
+            "contact": contact,
+            "ship_to": ship_to,
+            "delivery_method": delivery_method,
+        }
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    
+class PaymentView(View):
+    def get(self, request):
+        cart = request.session.get('cart', {})    
+        cart = request.session.get('cart', {}) 
+        subTotal = 0
+        items = []
+        for id, quantity in cart.items():
+            item = get_object_or_404(Candle, id=id)
+
+            if item.in_stock == 0 or quantity > item.in_stock:
+                # TODO: return to cart provide a message in cart
+                HttpResponseRedirect('/cart')
+
+            items.append({'item': item, 'quantity': quantity})
+            subTotal += item.price * quantity
+
+        return render(request, 'core_app/checkout/details.html', {'items': items, 'subTotal': subTotal})
+    
+    def post(self, request):
+        card_number = request.POST.get('card_number')
+        holder_name = request.POST.get('holder_name')
+        expiration = request.POST.get('expiration')
+        cvv = request.POST.get('cvv')
+
+        print({
+            "card_number": card_number,
+            "holder_name": holder_name,
+            "expiration": expiration,
+            "cvv": cvv
+        })
+
+        checkout = request.session.get('checkout', {})
+
+        checkout['payment'] = {
+            "card_number": card_number,
+            "holder_name": holder_name,
+            "expiration": expiration,
+            "cvv": cvv
+        }
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
